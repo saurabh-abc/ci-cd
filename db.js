fa91cf2802137c86e05ac66f90a221e0e6317832
saurabh <saurabhdhankar933@gmail.com>
2026-06-07 16:47:39 +0530

    initial commit for ci/cd

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/myapp');
+        await mongoose.connect('mongodb+srv://saurabhdhankar933_db_user:HrdufpX8WFqunr6f@cluster0.uffdxq0.mongodb.net/myTest1');
         console.log('MongoDB connected');
     } catch (err) {
         console.error(err.message);
